851fa432dd2d33cd54415b6be7e07a52326ece31
jakubgawor <kuba.gawor1@gmail.com>
2024-12-27 23:04:41 +0100

    added exception listener to handle exceptions and return json responses

---
+++ src/Shared/Listener/ExceptionListener.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Listener;
+
+use App\Shared\Request\Validator\ValidationError;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+
+final class ExceptionListener
+{
+    public function __construct(
+        private string $environment,
+    ) {
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof ValidationError) {
+            $code = Response::HTTP_BAD_REQUEST;
+            $content['errors'] = $exception->getErrors();
+        } else {
+            $code = Response::HTTP_INTERNAL_SERVER_ERROR;
+            if ($this->environment !== 'prod') {
+                $content['errors'] = [
+                    'server' => $exception::class . ' - ' . $event->getThrowable()->getMessage(),
+                ];
+            }
+        }
+
+        $event->setResponse(new JsonResponse($content ?? null, $code));
+    }
+}
