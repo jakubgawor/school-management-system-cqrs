0f91e7fabee48484997640842ee4ec9ae7057d02
jakubgawor <kuba.gawor1@gmail.com>
2024-12-28 17:53:15 +0100

    added migration adding user_verification_token table

---
+++ migrations/2024/12/Version20241228144638.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20241228144638 extends AbstractMigration
+{
+    public function up(Schema $schema): void
+    {
+        $this->addSql(
+            <<<SQL
+create table user_verification_token (
+    id char(36) not null,
+    user_id char(36) not null,
+    token varchar(255) not null,
+    created_at datetime not null,
+    expires_at datetime not null,
+    primary key(id),
+    constraint FK_user_verification_token_user_id foreign key (user_id) references user (id) on delete cascade
+)
+SQL
+        );
+
+        $this->addSql(
+            <<<SQL
+create index IDX_user_verification_token_id on user_verification_token (id)
+SQL
+        );
+
+        $this->addSql(
+            <<<SQL
+create index IDX_user_verification_token_user_id on user_verification_token (user_id)
+SQL
+        );
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql(
+            <<<SQL
+drop index IDX_user_verification_token_id on user_verification_token
+SQL
+        );
+
+        $this->addSql(
+            <<<SQL
+drop index IDX_user_verification_token_user_id on user_verification_token
+SQL
+        );
+
+        $this->addSql(
+            <<<SQL
+drop table user_verification_token
+SQL
+        );
+    }
+}
