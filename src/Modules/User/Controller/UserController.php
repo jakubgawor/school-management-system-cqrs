3344de5814c7d59c47705610d33f7e6268c1429a
jakubgawor <kuba.gawor1@gmail.com>
2024-12-28 19:42:52 +0100

    added endpoint to verify user email address with a token

--- src/Modules/User/Controller/UserController.php
+++ src/Modules/User/Controller/UserController.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace App\Modules\User\Controller;
 
-use App\Modules\User\Exception\UserAlreadyExistsException;
+use App\Modules\User\Exception\TokenDoesNotExists;
+use App\Modules\User\Exception\TokenExpired;
+use App\Modules\User\Exception\UserAlreadyExists;
 use App\Modules\User\Request\V1\UserRegister as UserRegisterRequestV1;
+use App\Modules\User\Request\V1\VerifyEmail as VerifyEmailRequestV1;
 use App\Shared\Command\Sync\CommandBus as SyncCommandBus;
 use App\Shared\Request\Validator\RequestValidator;
 use App\Shared\Request\Validator\ValidationError;
@@ -48,12 +51,48 @@ final class UserController extends AbstractController
 
         try {
             $this->syncCommandBus->dispatch($request->toCommand());
-        } catch (UserAlreadyExistsException) {
+        } catch (UserAlreadyExists) {
             throw new ValidationError([
                 ValidationError::GENERAL => ['VALIDATION.USER_ALREADY_EXISTS'],
             ]);
         }
 
         return new JsonResponse(null, Response::HTTP_CREATED);
+    }
+
+    #[Post(
+        summary: 'Verify user email with a token',
+        requestBody: new RequestBody(
+            required: true,
+            content: new JsonContent(
+                required: ['token'],
+                properties: [
+                    new Property(property: 'token', description: 'Verification token', type: 'string'),
+                ],
+                type: 'object'
+            )
+        ),
+        tags: ['User', 'v1']
+    )]
+    #[Route('/api/v1/user/verify_email', name: 'v1.user.verify_email', methods: ['POST'])]
+    public function verifyEmailV1(VerifyEmailRequestV1 $request): Response
+    {
+        $this->validator->validate($request);
+
+        try {
+            $this->syncCommandBus->dispatch($request->toCommand());
+        } catch (TokenExpired) {
+            throw new ValidationError([
+                ValidationError::GENERAL => ['VALIDATION.TOKEN_EXPIRED'],
+            ]);
+        } catch (TokenDoesNotExists) {
+            throw new ValidationError([
+                ValidationError::GENERAL => ['VALIDATION.TOKEN_NOT_FOUND'],
+            ]);
+        }
+
+        return new JsonResponse([
+            'status' => 'ok',
+        ], Response::HTTP_OK);
     }
 }
