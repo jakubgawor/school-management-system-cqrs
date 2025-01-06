a887a396cdaca1e752d3544930ef7c6da16a7feb
jakubgawor <kuba.gawor1@gmail.com>
2025-01-06 20:38:02 +0100

    added listener to enforce user email verification on restricted routes

---
+++ src/Modules/User/EventListener/VerifyUserListener.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\User\EventListener;
+
+use App\Modules\User\Exception\UserIsNotVerified;
+use Symfony\Bundle\SecurityBundle\Security;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+
+class VerifyUserListener
+{
+    private const array ALLOWED_ROUTES = [
+        'v1.user.verify_email',
+        'v1.user.resend_verification_code',
+        'v1.user.logout',
+    ];
+
+    public function __construct(
+        private Security $security,
+    ) {
+    }
+
+    public function isUserVerified(RequestEvent $event): void
+    {
+        if (in_array($event->getRequest()->attributes->get('_route'), self::ALLOWED_ROUTES, true)) {
+            return;
+        }
+
+        $user = $this->security->getUser();
+
+        if ($user && ! $user->isVerified()) {
+            throw new UserIsNotVerified();
+        }
+    }
+}
