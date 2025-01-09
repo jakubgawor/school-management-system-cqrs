bec1e068c85b7450ad21d572c567d545a96b44ff
jakubgawor <kuba.gawor1@gmail.com>
2025-01-09 22:15:35 +0100

    added openapi attributes

--- src/Modules/ClassRoom/Controller/ClassRoomController.php
+++ src/Modules/ClassRoom/Controller/ClassRoomController.php
@@ -10,10 +10,15 @@ use App\Modules\ClassRoom\Request\V1\CreateClassRoom as CreateClassRoomRequestV1
 use App\Shared\Command\Sync\CommandBus as SyncCommandBus;
 use App\Shared\Request\Validator\RequestValidator;
 use App\Shared\Request\Validator\ValidationError;
+use OpenApi\Attributes\Get;
+use OpenApi\Attributes\Items;
 use OpenApi\Attributes\JsonContent;
+use OpenApi\Attributes\Parameter;
 use OpenApi\Attributes\Post;
 use OpenApi\Attributes\Property;
 use OpenApi\Attributes\RequestBody;
+use OpenApi\Attributes\Schema;
+use OpenApi\Attributes\Response as OAResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -42,7 +47,7 @@ final class ClassRoomController extends AbstractController
         tags: ['ClassRoom', 'v1']
     )]
     #[Route('/api/v1/class_room/create', name: 'v1.class_room.create', methods: ['POST'])]
-    public function createClassRoomV1(CreateClassRoomRequestV1 $request): JsonResponse
+    public function createClassRoomV1(CreateClassRoomRequestV1 $request): Response
     {
         $this->validator->validate($request);
 
@@ -59,8 +64,66 @@ final class ClassRoomController extends AbstractController
         ], Response::HTTP_CREATED);
     }
 
+    #[Get(
+        summary: 'Get list of all class rooms',
+        parameters: [
+            new Parameter(
+                name: 'page',
+                description: 'Page number',
+                in: 'query',
+                required: false,
+                schema: new Schema(type: 'integer', example: 2)
+            ),
+            new Parameter(
+                name: 'limit',
+                description: 'Limit number of results',
+                in: 'query',
+                required: false,
+                schema: new Schema(type: 'integer', example: 20)
+            ),
+        ],
+        responses: [
+            new OAResponse(
+                response: 200,
+                description: 'Returns paginated list of all class rooms',
+                content: new JsonContent(
+                    properties: [
+                        new Property(property: 'page', type: 'integer', example: 2),
+                        new Property(property: 'limit', type: 'integer', example: 7),
+                        new Property(property: 'total', type: 'integer', example: 16),
+                        new Property(property: 'totalPages', type: 'integer', example: 3),
+                        new Property(
+                            property: 'data',
+                            type: 'array',
+                            items: new Items(
+                                properties: [
+                                    new Property(
+                                        property: 'id',
+                                        type: 'string',
+                                        format: 'uuid',
+                                        example: '01944cd2-235c-7245-85dc-79543815d1b7'
+                                    ),
+                                    new Property(
+                                        property: 'name',
+                                        type: 'string',
+                                        example: '1h'
+                                    ),
+                                    new Property(
+                                        property: 'createdAt',
+                                        type: 'string',
+                                        format: 'date-time',
+                                        example: '2025-01-09T12:34:56Z'
+                                    )
+                                ]
+                            )
+                        )
+                    ]
+                )
+            )
+        ]
+    )]
     #[Route('/api/v1/class_room/list', name: 'v1.class_room.list', methods: ['GET'])]
-    public function classRoomList(ClassRoomListQuery $classRoomListQuery): JsonResponse
+    public function classRoomList(ClassRoomListQuery $classRoomListQuery): Response
     {
         return new JsonResponse($classRoomListQuery->execute());
     }
