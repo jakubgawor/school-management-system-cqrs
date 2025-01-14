1b6dbca03485bf491367b8293341d8dd4b003645
jakubgawor <kuba.gawor1@gmail.com>
2025-01-14 22:03:17 +0100

    added teacher facade to check if the teacher is existing

---
+++ src/Modules/Teacher/Facade/TeacherFacade.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Teacher\Facade;
+
+use App\Modules\Teacher\Repository\TeacherRepository;
+
+final class TeacherFacade
+{
+    public function __construct(
+        private TeacherRepository $teacherRepository,
+    ) {
+    }
+
+    public function existsTeacherById(string $teacherId): bool
+    {
+        return (bool)$this->teacherRepository->findById($teacherId);
+    }
+}
